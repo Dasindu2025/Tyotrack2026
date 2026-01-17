e32e8b7acf93e30066d918c6913888c076ee1705
Dasindu Sithmira <lexmacsithmira@gmail.com>
2026-01-18 00:56:57 +0530

    fix: make vps_setup.sh non-interactive

--- vps_setup.sh
+++ vps_setup.sh
@@ -31,7 +31,7 @@ fi
 echo "🐳 Installing Docker & Docker Compose..."
 apt-get install -y ca-certificates curl gnupg lsb-release unzip
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
